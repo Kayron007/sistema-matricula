4fc1525599557250e7111b98fd6c5f8f08fe542b
Matheus <matheusalizsouza@gmail.com>
2025-10-19 22:57:42 -0300

    Padronizando métodos.

--- src/main/model/Turma.java
+++ src/main/model/Turma.java
@@ -2,7 +2,7 @@ package main.model;
 
 import java.time.Year;
 
-public class Turma extends BaseEntity {
+public class Turma extends Superclasse {
     private Year ano;
     private int semestre;
     private String id_curso; //FK
@@ -18,7 +18,6 @@ public class Turma extends BaseEntity {
         this.id_curso = id_curso;
     }
 
-    @Override
     public void mostrarDados() {
         System.out.println("=== Turma ===");
         System.out.println("ID: " + id);
@@ -34,6 +33,6 @@ public class Turma extends BaseEntity {
     public int getSemestre() { return semestre; }
     public void setSemestre(int semestre) { this.semestre = semestre; }
 
-    public String getIdcurso() { return id_curso; }
-    public void setIdcurso(String id_curso) { this.id_curso = id_curso; }
+    public String getIdCurso() { return id_curso; }
+    public void setIdCurso(String id_curso) { this.id_curso = id_curso; }
 }
